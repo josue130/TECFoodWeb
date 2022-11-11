f83fcaa5d15e3f67ce9fb8da2bdfc9928ceb9bb9
josue130 <josuelopez67i90@gmail.com>
2022-11-11 13:05:58 -0600

    Cascaron

---
+++ app.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const app = express();
+
+//2 - Para poder capturar los datos del formulario (sin urlencoded nos devuelve "undefined")
+app.use(express.urlencoded({extended:false}));
+app.use(express.json());//además le decimos a express que vamos a usar json
+
+//3- Invocamos a dotenv
+const dotenv = require('dotenv');
+dotenv.config({ path: './env/.env'});
+
+//4 -seteamos el directorio de assets
+app.use('/resources',express.static('public'));
+app.use('/resources', express.static(__dirname + '/public'));
+
+//5 - Establecemos el motor de plantillas
+app.set('view engine','ejs');
+
+const connection = require('./database/db');
+
+//9 - establecemos las rutas
+app.get('/login',(req, res)=>{
+    res.render('login');
+})
+
+app.get('/register',(req, res)=>{
+    res.render('register');
+})
+
+
+
+
+app.listen(3000, (req, res)=>{
+    console.log('SERVER RUNNING IN http://localhost:3000');
+});
+
